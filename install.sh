7dbb86f49dafbd2bfb97e53ced734ae5dc4afc8c
Mason Lapine <60404755+mwl4263@users.noreply.github.com>
2023-05-01 16:02:20 -0400

    Added more to install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,12 @@ pip3 install -r requirements.txt
 echo "Setting firewall permissions for port 3000..."
 sudo firewall-cmd --zone=public --add-port=3000/tcp --permanent
 sudo firewall-cmd --reload
+echo "enabling and starting mysqld..."
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
 echo "All tasks complete."
-echo "Run the app.py using the following commands:"
-echo "source venv/bin/activate"
-echo "python3 app.py"
+echo "Follow these steps to run the app:"
+echo "1. Make sure you have ran mysql_secure_installation and use password 'whatever' for the root user (all other options can be skipped by pressing the return key)."
+echo "2. 'Source db.sql' after running 'mysql -u root -p'"
+echo "3. source venv/bin/activate"
+echo "4. python3 app.py"
