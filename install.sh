3b0730a1d0169eb55218441e154add431676ef2a
Mason Lapine <60404755+mwl4263@users.noreply.github.com>
2023-05-01 15:16:43 -0400

    added logging to install.txt

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+echo "Installing dependencies from dnf..."
 sudo dnf update
 sudo dnf install mysql mysql-server python3.9
+echo "dnf dependencies installed."
+echo "making virtual environment..."
 mkdir venv
 python3.9 -m venv venv/
 source venv/bin/activate
+echo "Activated Virtual Environment."
+echo "Installing pip dependencies..."
 pip3 install -r requirements.txt
+echo "All tasks complete."
